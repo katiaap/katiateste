244984f9aa052ea904f1fc383a58d3de541028ff
Your Name <silva81.katia@gmail.com>
2020-07-22 22:35:59 -0300

    teste1tabuada

--- prjKatiaTeste/prjKatiaTeste/main.cpp
+++ prjKatiaTeste/prjKatiaTeste/main.cpp
@@ -1,7 +1,21 @@
-#include<iosream>
+#include<iostream>
 using namespace std;
 
 void main () {
+
+	int val1 = 0, val2 = 0;
+	
+	cout << "\ttabuada" << endl;
+	cout << "\t-------" << endl;
+	cout << "Enter value 1: ";
+	cin >> val1;
+	cout << "Enter value 2: ";
+	cin >> val2;
+
+
+	
+	cout << "Multiplication of " << val1 << " x " << val2 << " = " << (val1 * val2) << endl;
+	
 
 
 
